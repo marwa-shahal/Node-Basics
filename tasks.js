c09d00bdec0cc4c08f513fef4a91d5c51dcd8c59
Marwa <marwa.al.shahal@gmail.com>
2022-12-23 12:36:34 +0200

    better remove

--- tasks.js
+++ tasks.js
@@ -1,59 +1,53 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n'|| text==='exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
     quit();
-  }
-  else if(text.trim().split(" ")[0] === 'hello'){
+  } else if (text.trim().split(" ")[0] === "hello") {
     hello(text);
-  }else if(text === 'list\n'){
+  } else if (text === "list\n") {
     list();
-  }else if(text.trim().startsWith('add')){
+  } else if (text.trim().startsWith("add")) {
     add(text);
-  }else if(text.trim().startsWith('remove')){
+  } else if (text.trim().startsWith("remove")) {
     remove(text);
-  }
-  else if(text === 'help\n'){
+  } else if (text === "help\n") {
     help();
-  }
-  else{
+  } else {
     unknownCommand(text);
   }
 }
-
 
 /**
  * prints "unknown command"
@@ -62,30 +56,29 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(text){
-  console.log(text.trim() + "!")
+function hello(text) {
+  console.log(text.trim() + "!");
 }
 
-let tasks = ["do js exercise","study git"]
+let tasks = ["do js exercise", "study git"];
 /**
  * display tasks list
  *
  * @returns {void}
  */
-function list(){
-  tasks.map((task,index)=>{
-    console.log(index+1,task)
-  })
+function list() {
+  tasks.map((task, index) => {
+    console.log(index + 1, task);
+  });
 }
 
 /**
@@ -93,11 +86,11 @@ function list(){
  *
  * @returns {void}
  */
-function add(task){
-  if (task.trim()==="add"){
-    console.log("error")
-  }else{
-    tasks.push(task.trim().slice(3,).trim())
+function add(task) {
+  if (task.trim() === "add") {
+    console.log("error");
+  } else {
+    tasks.push(task.trim().slice(3).trim());
   }
 }
 
@@ -106,11 +99,15 @@ function add(task){
  *
  * @returns {void}
  */
-function remove(text){
-  if (text.trim() === "remove"){
-    tasks.splice(-1,1)
+function remove(text) {
+  if (text.trim() === "remove") {
+    tasks.splice(-1, 1);
   } else {
-    tasks.splice(parseInt(text.trim().split(" ")[1]) - 1 , 1)
+    if (parseInt(text.trim().split(" ")[1]) < tasks.length && parseInt(text.trim().split(" ")[1]) > 0) {
+      tasks.splice(parseInt(text.trim().split(" ")[1]) - 1, 1);
+    } else {
+      console.log("you entered a number that does not exist");
+    }
   }
 }
 
@@ -119,8 +116,10 @@ function remove(text){
  *
  * @returns {void}
  */
-function help(){
-  console.log('hello \n hello x\n quit \n list \n add x \n remove \n remove x\n')
+function help() {
+  console.log(
+    "hello \n hello x\n quit \n list \n add x \n remove \n remove x\n"
+  );
 }
 
 /**
@@ -128,11 +127,10 @@ function help(){
  *
  * @returns {void}
  */
-function quit(){
-  console.log('Quitting now, goodbye!')
+function quit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
-
 // The following line starts the application
-startApp("Marwa Al Shahal")
+startApp("Marwa Al Shahal");
